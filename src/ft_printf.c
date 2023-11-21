c6a0c2f13a8acffd00cea3e5a7e8aded2329a751
msloot <msloot@student.42.fr>
2023-11-21 20:03:27 +0100

    update `putnbr_base`

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 18:06:28 by msloot            #+#    #+#             */
-/*   Updated: 2023/11/18 18:56:04 by msloot           ###   ########.fr       */
+/*   Updated: 2023/11/21 20:02:31 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static ssize_t	ft_convert_p(va_list args)
 	ret1 = ft_putstr("0x");
 	if (ret1 < 0)
 		return (ret1);
-	ret2 = ft_putnbr_base(p, "0123456789abcdef");
+	ret2 = ft_putnbr_base(p, &(t_base){B16, B16_LEN});
 	if (ret2 < 0)
 		return (ret2);
 	return (ret1 + ret2);
@@ -57,9 +57,11 @@ static ssize_t	ft_convert(const char *input, size_t i, va_list args)
 	else if (input[i + 1] == 'c')
 		return (ft_putchar(va_arg(args, int)));
 	else if (input[i + 1] == 'x')
-		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef"));
+		return (ft_putnbr_base(
+				va_arg(args, unsigned int), &(t_base){B16, B16_LEN}));
 	else if (input[i + 1] == 'X')
-		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF"));
+		return (ft_putnbr_base(
+				va_arg(args, unsigned int), &(t_base){B16X, B16_LEN}));
 	else if (input[i + 1] == 'p')
 		return (ft_convert_p(args));
 	else if (input[i + 1] == '%')
